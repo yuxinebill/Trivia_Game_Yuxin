70e075e9bfb9893b8bc0736259accf59077f5f4c
yuxin <yuxinhe2016@gmail.com>
2018-06-07 04:36:54 -0400

    next button

--- assets/JS/myJS.js
+++ assets/JS/myJS.js
@@ -26,6 +26,12 @@ $(document).ready(function() {
 			"question" : "What does Yuxin do currently?",
 			"answers"  : ["Graphic Designer", "Real Estate Agent", "Makeup Artist", "Saleswoman"],
 			"rightAnswer" : "Real Estate Agent"
+		},
+
+		{	"id" : 4,
+			"question" : "How many cats does Yuxin have?",
+			"answers"  : ["0", "1", "2", "3"],
+			"rightAnswer" : "1"
 		}
 
 	];
@@ -35,42 +41,53 @@ $(document).ready(function() {
 
 	//startFunction will make the start button invisible and display the first question on the page
 	function startFunction () {
-		$(".startButton").addClass("invisible");
+		$(this).remove();
 		displayQuestion ();
 	};
 
 	var i = 0 ;
 
 	function displayQuestion () {
+
 		//list question on screen
 		$(".question").text(myData[i].question);
 
 		//list all answers on screen
 		for (k=0; k<myData[i].answers.length; k++) {
 			var eachAnswer = myData[i].answers[k];
+			console.log(eachAnswer)
 
 			$("<div>").addClass("form-check form-group").addClass(eachAnswer).appendTo($(".answers"));
 			$("." + eachAnswer).append($("<input type='radio'/>").addClass("form-check-input").attr("value", eachAnswer));
 			$("." + eachAnswer).append($("<label>").addClass("form-check-label").text(eachAnswer));
 		}
 		//when user check the checkbox, then run the function
-		$("input").change(function() {
+		$("input").change(function() {			
 		    if(this.checked) {
+		    	function nextButton () {
+		    		$("<button>").addClass("btn btn-info mt-5 nextButton").attr("type", "button").appendTo($(".answers")).text("NEXT >");
+		    	}
 		    	//if the user choose the right answer, then show "Yeah, you know me!" and show next button
 		    	//if the user choose wrong ones, then show "The answer is xxx", and show next button
 		        if (this.value == myData[i].rightAnswer ) {
-		        	$(".answers").text("Yeah, you know me!")
+		        	$(".answers").text("Yeah, you know me!").append('<br>');
+		        	nextButton ();
 		        } else {
-		        	$(".answers").text("The answer is " + myData[i].rightAnswer)
+		        	$(".answers").text("The answer is " + myData[i].rightAnswer ).append('<br>');
+		        	nextButton ();
 		        }
 		    }
 		    i++;
 		});		
 	};
 
-	
-
 	$(".startButton").on("click", startFunction);
+
+	$(document).on("click", ".nextButton", function(){
+		$(this).remove();
+		$(".answers").empty();
+		displayQuestion ();
+	});
 
 });
 
