ed977b83e1537b40e36cdd2fa79fbd73d1439bcf
yuxin <yuxinhe2016@gmail.com>
2018-06-07 03:45:18 -0400

    checkbox function ready

--- assets/JS/myJS.js
+++ assets/JS/myJS.js
@@ -50,12 +50,25 @@ $(document).ready(function() {
 			var eachAnswer = myData[i].answers[k];
 
 			$("<div>").addClass("form-check form-group").addClass(eachAnswer).appendTo($(".answers"));
-			$("." + eachAnswer).append($("<input type='radio'/>").addClass("form-check-input"));
+			$("." + eachAnswer).append($("<input type='radio'/>").addClass("form-check-input").attr("value", eachAnswer));
 			$("." + eachAnswer).append($("<label>").addClass("form-check-label").text(eachAnswer));
 		}
-
-		i++;
+		//when user check the checkbox, then run the function
+		$("input").change(function() {
+		    if(this.checked) {
+		    	//if the user choose the right answer, then show "Yeah, you know me!" and show next button
+		    	//if the user choose wrong ones, then show "The answer is xxx", and show next button
+		        if (this.value == myData[i].rightAnswer ) {
+		        	$(".answers").text("Yeah, you know me!")
+		        } else {
+		        	$(".answers").text("The answer is " + myData[i].rightAnswer)
+		        }
+		    }
+		    i++;
+		});		
 	};
+
+	
 
 	$(".startButton").on("click", startFunction);
 
